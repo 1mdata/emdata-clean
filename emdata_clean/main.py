a6dabbcf532f5105585fe5a0b78d6840ad46b2ef
cyrus <cyrusgu2016@gmail.com>
2017-02-07 10:00:37 +0800

    add main.py

---
+++ emdata_clean/main.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+    emdata_clean.main
+    ~~~~~~~~~
+
+"""
+
+import click
+
+
+class Config(object):
+    """docstring for Config."""
+
+    def __init__(self, arg):
+        super(Config, self).__init__()
+        self.verbose = False
+
+pass_config = click.make_pass_decorator(Config)
+
+
+@click.group()
+@click.option('--verbose', is_flag=True)
+def cli(verbose):
+    if verbose:
+        print('We are in verbose mode')
+
+
+@cli.command('d', short_help='Runs a datas delete.')
+@click.option('--string', default='World',
+              help='This the thing that is greeted.')
+@click.option('--repeat', default=1,
+              help='How many times you should be greeted.')
+@click.argument('out', type=click.File('w'), default='-',
+                required=False)
+def delete(string, repeat):
+    for x in range(repeat):
+        print('Hello %s!' % string, file=out)
